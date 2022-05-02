e747cbe94a63e4416d14ae21d391653bac722926
ben.li <ben.li@rightcrowd.com>
2022-05-02 15:50:16 +1000

    try to add route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from "react";
-import Movies from "./components/movies";
-import "./App.css";
+import React, { Component } from 'react'
+import Movies from './components/movies'
+import { Route, Switch, Redirect } from 'react-router-dom'
+import NotFound from './components/notFound'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/" exact component={Movies} />
+          {/* order from most spesific one to most generaic one */}
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
-    );
+    )
   }
 }
 
-export default App;
+export default App
