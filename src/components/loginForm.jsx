cbb0b6c9aba4d69f0d5d8f696c05c351d33e7dbc
Ben <ben.li19930119@gmail.com>
2022-05-08 01:09:40 +1000

    add basic login form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,19 +1,59 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  //state = {  }
+  state = {
+    account: { username: "", password: "" },
+  };
+  // username = React.createRef(); // create ref obejct, should minmize of using this, only use when know what I am doing // use for animation , etc
+
+  // Add life cycle hook:
+  //  componentDidMount() {
+  //    this.username.current.focuse();
+  //  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+
+    // const username = this.username.current.value;
+
+    // the whol point of react is put abstraction over DOM document object model
+    // so never use document.getElementById
+    console.log("Submited");
+  }
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              name="username"
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
